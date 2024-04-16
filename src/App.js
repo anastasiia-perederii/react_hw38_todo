eff25aa698efd89acf911e2d0d4514b661f563dc
Anastasiia <anastasiia.perederii@gmail.com>
2024-04-16 18:35:04 +0300

    version  1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        const [todos, setTodos] = useState([]);
+        const [inputValue, setInputValue] = useState('');
+
+        const handleInputChange = (event) => {
+            setInputValue(event.target.value);
+        };
+
+        const handleAddTodo = () => {
+            if (inputValue.trim() !== '') {
+                const newTodo = {
+                    id: Date.now(),
+                    text: inputValue,
+                    completed: false,
+                };
+                setTodos([...todos, newTodo]);
+                setInputValue('');
+            }
+        };
+
+        const handleToggleTodo = (id) => {
+            setTodos(
+                todos.map((todo) =>
+                    todo.id === id ? { ...todo, completed: !todo.completed } : todo
+                )
+            );
+        };
+
+        const handleDeleteTodo = (id) => {
+            setTodos(todos.filter((todo) => todo.id !== id));
+        };
+
+        return (
+            <div>
+                <h1>Todo List</h1>
+                <ul>
+                    {todos.map((todo) => (
+                        <li key={todo.id}>
+            <span
+                style={{
+                    textDecoration: todo.completed ? 'line-through' : 'none',
+                    cursor: 'pointer',
+                }}
+                onClick={() => handleToggleTodo(todo.id)}
+            >
+              {todo.text}
+            </span>
+                            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+                <div>
+                    <input
+                        type="text"
+                        value={inputValue}
+                        onChange={handleInputChange}
+                        placeholder="Enter todo..."
+                    />
+                    <button onClick={handleAddTodo}>Add Todo</button>
+                </div>
+            </div>
+        );
+    }
 
 export default App;
